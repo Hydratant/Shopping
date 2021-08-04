db436c3576632b53775a99661d6e38315e908322
박상혁 <psh161@naver.com>
2021-08-04 17:35:46 +0900

    - Good Data Model 변경

--- app/src/main/java/com/tami/shopping/entity/GoodEntity.kt
+++ app/src/main/java/com/tami/shopping/entity/GoodEntity.kt
@@ -4,28 +4,18 @@ import com.tami.shopping.model.GoodData
 
 data class GoodEntity(
     val id: Int,            // Id
-    val actual_price: Int,  // 세일 가격
+    val actual_price: Int,  // 실제 가격
     val image: String,      // Image Url
     val is_new: Boolean,    // 신상 여부
     val name: String,       // 이름
-    val price: Int,         // 가격
+    val price: Int,         // 판매 가격
     val sell_count: Int     // 판매수
 ) {
     companion object {
         val empty = GoodEntity(0, 0, "", true, "", 0, 0)
     }
 
-    fun toGoodData(): GoodData {
-
-        // 가격
-        val price = if (price == actual_price) price
-        else actual_price
-
-        // 세일율 구하기
-        val salePercent = if (price == actual_price) 0
-        else (price - actual_price) / price
-
-        return GoodData(id, name, image, is_new, price.toString(), sell_count.toString(), salePercent.toString())
-    }
+    fun toGoodData(): GoodData =
+        GoodData(id, actual_price, image, is_new, name, price, sell_count)
 
 }
--- app/src/main/java/com/tami/shopping/model/GoodData.kt
+++ app/src/main/java/com/tami/shopping/model/GoodData.kt
@@ -2,15 +2,30 @@ package com.tami.shopping.model
 
 
 data class GoodData(
-    val id: Int,                // Id
-    val name: String,           // 이름
-    val image: String,          // Image Url
-    val isNew: Boolean,         // 신상 여부
-    val price: String,          // 가격
-    val sellCount: String,      // 구매수
-    val salePercent: String,    // 세일 Percent
+    val id: Int,            // Id
+    val actualPrice: Int,   // 실제 가격
+    val image: String,      // Image Url
+    val isNew: Boolean,     // 신상 여부
+    val name: String,       // 이름
+    val price: Int,         // 판매 가격
+    val sellCount: Int      // 판매수
 ) {
     companion object {
-        val empty = GoodData(0, "", "", true, "", "", "")
+        val empty = GoodData(0, 0, "", true, "", 0, 0)
     }
+
+    val salePercent: String
+        get() {
+            val percent = if (actualPrice == price) 0
+            else ((actualPrice - price) / actualPrice)
+            return percent.toString()
+        }
+
+
+    val showPrice: String
+        get() {
+            val showPrice = if (actualPrice == price) actualPrice
+            else price
+            return showPrice.toString()
+        }
 }
