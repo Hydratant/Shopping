b6b0bd19a8bcf20a5e433fc94739d337d87628d0
박상혁 <psh161@naver.com>
2021-08-05 09:26:21 +0900

    - Favorite Repository

--- app/src/main/java/com/tami/shopping/data/repo/FavoriteRepositoryImpl.kt
+++ app/src/main/java/com/tami/shopping/data/repo/FavoriteRepositoryImpl.kt
@@ -6,15 +6,13 @@ import com.tami.shopping.model.GoodData
 class FavoriteRepositoryImpl constructor(
     private val favoriteDataSource: FavoriteDataSource
 ) : FavoriteRepository {
-    override suspend fun getFavoriteList(): List<GoodData> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getFavoriteList(): List<GoodData> =
+        favoriteDataSource.getFavoriteList().map { it.toGooData() }
 
     override suspend fun insertFavorite(goodData: GoodData) {
         favoriteDataSource.insertFavorite(goodData.toFavoriteGoodEntity())
     }
 
-    override suspend fun deleteFavorite(id: Int): Int {
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteFavorite(id: Int): Int =
+        favoriteDataSource.deleteFavorite(id)
 }
