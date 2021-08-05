47e9edf6dba3107f1e9d2d9ca93433a7fb41b7d9
박상혁 <psh161@naver.com>
2021-08-05 09:18:08 +0900

    - good Entity

--- app/src/main/java/com/tami/shopping/entity/FavoriteGoodEntity.kt
+++ app/src/main/java/com/tami/shopping/entity/FavoriteGoodEntity.kt
@@ -3,15 +3,19 @@ package com.tami.shopping.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.tami.shopping.model.GoodData
 
 @Entity(tableName = "favorites")
 data class FavoriteGoodEntity(
     @PrimaryKey
-    @ColumnInfo(name = "id")          val id: Int = 0,        // Id
-    @ColumnInfo(name = "actualPrice") val actualPrice: Int,   // 실제 가격
-    @ColumnInfo(name = "image")       val image: String,      // Image Url
-    @ColumnInfo(name = "isNew")       val isNew: Boolean,     // 신상 여부
-    @ColumnInfo(name = "name")        val name: String,       // 이름
-    @ColumnInfo(name = "price")       val price: Int,         // 판매 가격
-    @ColumnInfo(name = "sellCount")   val sellCount: Int,     // 판매수
-)
+    @ColumnInfo(name = "id")            val id: Int = 0,        // Id
+    @ColumnInfo(name = "actualPrice")   val actualPrice: Int,   // 실제 가격
+    @ColumnInfo(name = "image")         val image: String,      // Image Url
+    @ColumnInfo(name = "isNew")         val isNew: Boolean,     // 신상 여부
+    @ColumnInfo(name = "name")          val name: String,       // 이름
+    @ColumnInfo(name = "price")         val price: Int,         // 판매 가격
+    @ColumnInfo(name = "sellCount")     val sellCount: Int,     // 판매수
+) {
+    fun toGooData(): GoodData =
+        GoodData(id, actualPrice, image, isNew, name, price, sellCount, true)
+}
